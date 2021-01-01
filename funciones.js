952221f805021a1fc7e7c727cae41069850af250
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2021-01-01 23:11:39 +0100

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -1,17 +1,17 @@
 
 
-function esNumero(num){
+function esNumero(num) {
     return !isNaN(num);
 }
 
-function generarAleatorio(minimo, maximo){
+function generarAleatorio(minimo, maximo) {
 
-    if(esNumero(minimo) && esNumero(maximo)){
+    if (esNumero(minimo) && esNumero(maximo)) {
 
         minimo = parseInt(minimo);
         maximo = parseInt(maximo);
-        
-        if(minimo > maximo ){
+
+        if (minimo > maximo) {
             let aux = maximo;
             maximo = minimo;
             minimo = aux;
@@ -19,9 +19,93 @@ function generarAleatorio(minimo, maximo){
 
         return Math.floor(Math.random() * (maximo - minimo)) + minimo;
 
-    }else{
+    } else {
         console.error("Los numeros no tienen un formato correcto")
     }
 
 
 }
+
+
+function sumaArray(array) {
+
+    let total = 0;
+
+    if (Array.isArray(array)) {
+
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+
+            if (esNumero(element)) {
+                total += element;
+            }
+
+        }
+
+    } else {
+        console.error("Debes pasar un array como parametro");
+    }
+
+    return total;
+}
+
+function mediaArray(array) {
+
+    let media = 0;
+
+    if (Array.isArray(array)) {
+
+        let suma = sumaArray(array);
+
+        media = suma / array.length;
+
+    } else {
+        console.error("Debes pasar un array como parametro");
+    }
+
+    return media;
+
+}
+
+function exists(array, elemento) {
+
+    if (Array.isArray(array)) {
+
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+
+            if (element == elemento) {
+                return true;
+            }
+        }
+
+        // if (array.find(e => e == elemento)) {
+        //     return true;
+        // }
+
+    } else {
+        console.error("Debes pasar un array como parametro");
+    }
+
+    return false;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
